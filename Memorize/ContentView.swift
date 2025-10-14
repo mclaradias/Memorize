34e36577f56106bc9d0e0838f7d5a53bddfadce2
Maria Clara <mariaclara.dg25@gmail.com>
2025-10-14 09:58:26 -0300

    Lesson 1: Course Logistics and Introduction to SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        HStack(){
+            ForEach(0..<4) { index in
+                CardView()
+            }
         }
         .padding()
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color(.white))
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke()
+                Text("👻")
+                    .font(.system(size: 80))
+            } else {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color(.orange))
+            }
+        }
     }
 }
 
